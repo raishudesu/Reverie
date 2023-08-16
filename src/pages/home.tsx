940e1f13b3be120fe35c6ec67f0b36a2267d1b69
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-08-16 23:09:13 +0800

    updated home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Home = () => {
   return (
     <div className="w-full h-screen flex justify-center">
-      <div className="overflow-hidden rounded-lg w-full mt-16 max-h-fit md:w-[65%] flex justify-center lg:border ">
+      <div className="overflow-hidden rounded-lg w-full mt-16 max-h-fit md:w-[65%] flex justify-center md:border ">
         <HomeSidePanel />
         <Outlet />
       </div>
